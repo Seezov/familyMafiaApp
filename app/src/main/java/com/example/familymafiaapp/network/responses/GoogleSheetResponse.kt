7245a79cd309623ca45490056bf9456ad588aa6d
Seezov <oleksandr.sizov@ekreative.com>
2021-01-06 17:05:41 +0200

    Remove redundant code

--- app/src/main/java/com/example/familymafiaapp/network/responses/GoogleSheetResponse.kt
+++ app/src/main/java/com/example/familymafiaapp/network/responses/GoogleSheetResponse.kt
@@ -1,14 +1,10 @@
 package com.example.familymafiaapp.network.responses
 
-import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class GoogleSheetResponse(
-    @field:Json(name = "range")
     var range: String,
-    @field:Json(name = "majorDimension")
     var majorDimension: String,
-    @field:Json(name = "values")
     var values: List<List<String>>
 )
